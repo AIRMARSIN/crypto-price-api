c2714dae15b3e6ce686920f0bb6dd25c2a1c8375
airmarsin <veme1900@gmail.com>
2026-01-21 13:05:38 +0100

    made a minor update to jobs/priceUpdater.js

--- jobs/priceUpdater.js
+++ jobs/priceUpdater.js
@@ -2,13 +2,11 @@ const cron = require("node-cron");
 const { fetchAndCachePrices } = require("../services/cryptoService");
 
 function startPriceUpdater() {
-  // Every 60 seconds
-  cron.schedule("*/60 * * * * *", async () => {
+  // Every 3 minutes
+  cron.schedule("*/3 * * * *", async () => {
     console.log("Updating crypto prices...");
     await fetchAndCachePrices();
   });
 }
 
-module.exports = {
-  startPriceUpdater
-};
+module.exports = { startPriceUpdater };
