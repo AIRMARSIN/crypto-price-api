f7c847d9c47f53ee7b58c7fb70c87d9b2d638af9
airmarsin <veme1900@gmail.com>
2026-02-02 12:21:21 +0100

    Merge remote changes

--- jobs/priceUpdater.js
+++ jobs/priceUpdater.js
@@ -2,8 +2,8 @@ const cron = require("node-cron");
 const { fetchAndCachePrices } = require("../services/cryptoService");
 
 function startPriceUpdater() {
-  // Every 3 minutes
-  cron.schedule("*/3 * * * *", async () => {
+  // Every 10 minutes (CoinGecko free tier ~10 calls/min)
+  cron.schedule("*/10 * * * *", async () => {
     console.log("Updating crypto prices...");
     await fetchAndCachePrices();
   });
